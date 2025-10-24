084d43ef1b9429a5ae0b09ad188ecdc0ed82e619
Kirill <kpetuhov3242@gmail.com>
2025-10-24 22:08:46 +0300

    rename main java lab2

--- java/lab2/Main.java
+++ java/lab2/Main.java
@@ -1,7 +1,7 @@
 import ru.ifmo.se.pokemon.*;
 import mypokemons.*;
 
-public class MainL2 {
+public class Main {
     public static void main(String[] args) {
         Battle b = new Battle();
         Pokemon p1 = new Alomomola("Аламола");
