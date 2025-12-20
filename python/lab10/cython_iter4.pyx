c7bc6737f31fb87c8fb7013e2e7e706793f941ba
Kirill <kpetuhov3242@gmail.com>
2025-12-20 23:34:56 +0300

    python

--- python/lab10/cython_iter4.pyx
+++ python/lab10/cython_iter4.pyx
@@ -1,5 +1,5 @@
 
-cpdef float integrate(f, float a, float b, int n_iter=1000):
+cpdef float integrate(object f, float a, float b, int n_iter=1000):
     """
     Интегрирует методом прямоугольников.
     
@@ -12,15 +12,10 @@ cpdef float integrate(f, float a, float b, int n_iter=1000):
     Returns:
         float: значение вычисленного интеграла.
     """
-    cdef float acc = 0
+    cdef float acc, step
+    cdef int i
+    acc = 0
     step = (b - a) / n_iter
     for i in range(n_iter):
         acc += f(a + i*step) * step
     return acc
-
-
-if __name__ == "__main__":
-    for i in range(5):
-        print(f"===={10**i*100} итераций====")
-        print(sum(timeit.repeat(lambda: integrate(math.sin, 0, math.pi / 2, n_iter=10**i*100), number=100//(10**(i==4)), repeat=5))/5)
-    
---
+++ python/lab10/setup_iter4.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from Cython.Build import cythonize
+
+setup(
+    ext_modules=cythonize(r"C:\Users\k4484\itmo\itmo-1\python\lab10\cython_iter4.pyx"),
+)
+
+# python .\python\lab10\setup_iter4.py build_ext --inplace
+# python .\setup_iter4.py build_ext --inplace
